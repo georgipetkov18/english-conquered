387cafd9f56e7feb17c3e651611b9a0d970c334e
Georgi Petkov <george18petkov@gmail.com>
2022-05-09 09:19:48 +0300

    Implement full CRUD for modules

--- EnglishConquered/api/views/module.py
+++ EnglishConquered/api/views/module.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from rest_framework import status, views
+from api.serializers.module_serializer import ModuleSerializer
 from data.repositories import ModuleRepository
 from business.models import Module
 
@@ -7,6 +8,12 @@ class ModuleView(views.APIView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.module_repo = ModuleRepository()
+
+
+    def get(self, request, format=None):
+        modules = self.module_repo.get_all()
+        data = [ModuleSerializer(module).data for module in modules]
+        return JsonResponse({'modules': data}, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
         unit = request.data.get('unit')
@@ -28,4 +35,33 @@ class ModuleDetailView(views.APIView):
         self.module_repo = ModuleRepository()
 
     def get(self, request, id, format=None):
-        pass
+        try:
+            module = self.module_repo.get(id)
+            return JsonResponse({'module': ModuleSerializer(module).data}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def put(self, request, id, format=None):
+        unit = request.data.get('unit')
+        if unit == None:
+            return JsonResponse({'error': 'The unit field is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            updated_module = self.module_repo.update(id, Module(unit))
+            return JsonResponse({'updated_module': ModuleSerializer(updated_module).data}, status=status.HTTP_200_OK)
+ 
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+    def delete(self, request, id, format=None):
+        try:
+            self.module_repo.delete(id)
+            return JsonResponse({'message': 'Module deleted successfully'}, status=status.HTTP_200_OK)
+ 
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        
--- EnglishConquered/data/repositories/module_repository.py
+++ EnglishConquered/data/repositories/module_repository.py
@@ -3,14 +3,16 @@ from business import models
 
 class ModuleRepository:
     def get_all(self):
-        return Module.objects.filter(deleted_on__isnull=False).order_by('-unit')
+        return Module.objects.filter(deleted_on__isnull=True).order_by('-unit')
+
 
     def get(self, id):
-        module = Module.objects.filter(id=id)
+        module = Module.objects.filter(id=id)[0]
         if not module:
             raise Exception('Invalid id was provided')
 
         return module
+
 
     def add(self, module: models.Module):
         if module.unit <= 0:
@@ -18,8 +20,15 @@ class ModuleRepository:
 
         Module.objects.create(unit=module.unit)
 
-    def update(self, id, module):
-        pass
+
+    def update(self, id, module: models.Module):
+        if module.unit <= 0:
+            raise Exception('Unit must be a positive integer')
+
+        Module.objects.filter(id=id).update(unit=module.unit)
+        return Module.objects.filter(id=id)[0]
+
 
     def delete(self, id):
-        pass
+        Module.objects.filter(id=id).delete()
+
